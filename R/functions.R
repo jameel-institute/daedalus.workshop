74a93be70fef2aed537aa5e826dfc1a08151585d
Pratik Gupte <pratikgupte16@gmail.com>
2026-06-30 14:54:45 +0100

    WIP: add table, update doc

--- R/functions.R
+++ R/functions.R
@@ -1,13 +1,18 @@
 #' Generate all workshop materials
 #'
 #' Generates all workshop materials in the current working directory.
+#'
+#' @return
+#'
+#' @export
 make_materials <- function(
   country = "GBR",
   disease = "sars_cov_1",
   r0 = 3.0,
   t0 = 30,
   horizon = 100,
-  n_samples = 10
+  n_samples = 10,
+  render = TRUE
 ) {
   cli::cli_inform(
     "Generating workshop materials at dir {.file {here::here()}}"
@@ -15,24 +20,35 @@ make_materials <- function(
 
   # makes figures - unsure if wanted
   # make_projections(country, disease, r0, t0, t_end)
+  fig_path <- fs::path_abs("figures")
+  tables_path <- fs::path_abs("tables")
+
+  fs::dir_create(fig_path)
+  fs::dir_create(tables_path)
 
   # makes handout
   rmarkdown::draft(
     "handout",
     "handout",
     "daedalus.workshop",
-    TRUE
+    TRUE,
+    edit = FALSE
   )
-  rmarkdown::render(
-    file.path("handout", "handout.Rmd"),
-    params = list(
-      country = country,
-      base_disease = disease,
-      t0 = t0,
-      horizon = horizon,
-      n_samples = n_samples
+
+  if (render) {
+    rmarkdown::render(
+      file.path("handout", "handout.Rmd"),
+      params = list(
+        country = country,
+        base_disease = disease,
+        t0 = t0,
+        horizon = horizon,
+        n_samples = n_samples,
+        figures_out = fig_path,
+        tables_out = tables_path
+      )
     )
-  )
+  }
 
   # makes phase 1 presentation (may not be used)
   # rmarkdown::draft(
