4b92a4aee3ab1c3693014dd5d10ce589fcd6958b
Pratik Gupte <pratikgupte16@gmail.com>
2026-07-27 13:18:31 +0100

    Fixes from review

--- R/helpers.R
+++ R/helpers.R
@@ -1,11 +1,10 @@
 #' Check for LaTeX
 #'
-#' @return A single logical for whether LaTeX (and specifically PdfLaTeX) is
-#' available. Also has warning side-effects informing users about missing LaTeX
-#' and PdfLaTeX.
+#' @return Warning side-effects informing users about missing LaTeX
+#' and specifically PdfLaTeX.
 #'
-#' @export
-has_pdflatex <- function() {
+#' @keywords internal
+check_pdflatex <- function() {
   latex_typesetters <- c("pdflatex", "xelatex", "lualatex")
 
   ltx_avail <- Sys.which(latex_typesetters)
@@ -14,18 +13,17 @@ has_pdflatex <- function() {
   if (any_avail) {
     has_pdflatex <- nzchar(ltx_avail["pdflatex"])
     if (!has_pdflatex) {
+      which_available <- latex_typesetters[nzchar(ltx_avail)] # nolint used cli
+
       cli::cli_warn(
         "pdflatex was not found on this system --- there may be errors in PDF \
-        output rendering. Set the LaTeX engine to the typesetter available, \
-        such as XeLaTeX or LuaLaTeX.
+        output rendering. Other LaTeX typsetters are available on this system: \
+        {.str {which_available}}.
+        Set the LaTeX engine to the typesetter available.
         See the {.pkg rmarkdown} documentation on LaTeX options: \
         https://pkg.yihui.org/rmarkdown-book/pdf-document#latex-options"
       )
-
-      FALSE
     }
-
-    TRUE
   } else {
     cli::cli_warn(
       "No LaTeX installation detected on this system; PDF output probably \
@@ -33,7 +31,5 @@ has_pdflatex <- function() {
       render documents manually.
       Consider installing the package {.pkg tinytex}."
     )
-
-    FALSE
   }
 }
